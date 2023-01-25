90c4d8267e318128cfcdef12bd764dd33f9c7894
masontrang <mt9260@gmail.com>
2023-01-25 17:23:01 -0600

    add MealsOverViewScreen and CategoriesScreen

--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -1,6 +1,33 @@
-import { Text, View, Pressable } from 'react-native';
+import { Text, View, Pressable, Button, FlatList } from 'react-native';
 
 import CategoryGridTile from '../components/CategoryGridTile';
-function CategoriesScreen() {}
+import { CATEGORIES } from '../data/dummy-data';
+
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler({ navigate }) {
+      navigation.navigate('MealsOverviewScreen', {
+        categoryId: itemData.item.id,
+      });
+    }
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+  return (
+    <View>
+      <FlatList
+        data={CATEGORIES}
+        renderItem={renderCategoryItem}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+      />
+    </View>
+  );
+}
 
 export default CategoriesScreen;
--- screens/MealsOverviewScreen.jsx
+++ screens/MealsOverviewScreen.jsx
@@ -1,6 +1,36 @@
-import { Text, View, Pressable } from 'react-native';
+import { Text, View, Pressable, FlatList } from 'react-native';
 import MealItem from '../components/MealItem';
 
-function MealsOverviewScreen() {}
+import { MEALS } from '../data/dummy-data';
+
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.indexOf(catId) >= 0;
+  });
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+    return (
+      <MealItem
+        title={item.title}
+        imageUrl={item.imageUrl}
+        affordability={item.affordability}
+        complexity={item.complexity}
+        duration={item.duration}
+      />
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+}
 
 export default MealsOverviewScreen;
